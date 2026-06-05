ee885e4b562f1d96b0f7691835b09a2189db14be
Marcos Garcia <jr329garcia@gmail.com>
2026-06-04 19:53:10 -0700

    board visual generated

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,20 @@
 
 #include <iostream>
 
+// Define Graphics
+
+const int TILE_SIZE = 30;
+const int TILE_SPACING = 30;
+
+//
+
 int main() {
   const int X_BOARD = 15;
   const int Y_BOARD = 15;
   const int BOMBS = 30;
   Board test_board(X_BOARD, Y_BOARD, BOMBS);
+
+
 
   InitWindow(800, 800, "Markerses Minesweeper");
 
@@ -34,6 +43,19 @@ int main() {
 
   while (!WindowShouldClose()) {
     BeginDrawing();
+
+    ClearBackground(BLACK);
+    DrawRectangle(180, 180, 490, 490, GRAY);
+
+    for (size_t i = 0; i < X_BOARD; i++) {
+      for (size_t j = 0; j < Y_BOARD; j++) {
+        int x_pos = 200 + (i * TILE_SPACING);
+        int y_pos = 200 + (j * TILE_SPACING);
+        Color col = (test_board.ShowBoard()[i][j]->TileNumber() == -1) ? RED:GREEN;
+
+        DrawRectangle(x_pos, y_pos, TILE_SIZE, TILE_SIZE, col);
+      }
+    }
 
     EndDrawing();
   }
